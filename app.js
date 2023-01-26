aafad55867a79e6a9bd982decc7f0e3523fa04c4
Ahmed AbdElsalam <91109569+Genius8545@users.noreply.github.com>
2023-01-26 18:26:21 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const AppError = require('./utils/appError');
+const errControllers = require('./controllers/errControllers');
+const userRouter = require('./routes/userRoutes');
+const equipmentsRouter  = require('./routes/equipmentsRoutes');
+const app = express();
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+app.use(express.json());
+// app.use(express.static(`${__dirname}/public`));
+// app.use((req, res, next) => {
+//   req.requestTime = new Date().toISOString();
+//   console.log(req.headers);
+//   next();
+// })
+
+
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/Equipments', equipmentsRouter);
+
+
+// handle unhandled routes
+//firts goes to app error 
+app.all('*', (req, res, next) => {
+  next(new AppError(`can't found ${req.originalUrl}`, 404)); // class inheritance 
+});
+
+//then errControllers
+app.use(errControllers);
+
+module.exports = app;
