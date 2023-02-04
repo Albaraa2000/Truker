46b9e3d3a1d15bc49e73f5967d2a80ad3cddc702
genius8545 <ahmedabdelsalm6@gmail.com>
2023-02-04 21:49:54 +0200

    edit status code

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,8 +1,8 @@
-const { promisify } = require('util');
-const jwt = require('jsonwebtoken');
-const User = require('./../models/userModel');
-const catchAsync = require('./../utils/catchAsync');
-const AppError = require('./../utils/appError');
+const { promisify } = require("util");
+const jwt = require("jsonwebtoken");
+const User = require("./../models/userModel");
+const catchAsync = require("./../utils/catchAsync");
+const AppError = require("./../utils/appError");
 
 const signToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
@@ -19,11 +19,9 @@ exports.signup = catchAsync(async (req, res, next) => {
   });
   const token = signToken(newUser._id);
   res.status(201).json({
-    status: 'success',
+    status: "success",
     token,
-    data: {
-      user: newUser,
-    },
+    newUser,
   });
 });
 exports.login = catchAsync(async (req, res, next) => {
@@ -31,20 +29,21 @@ exports.login = catchAsync(async (req, res, next) => {
 
   // 1) Check if email and password exist
   if (!email || !password) {
-    return next(new AppError('Please provide email and password!', 400));
+    return next(new AppError("Please provide email and password!", 200));//400
   }
   // // 2) Check if user exists && password is correct
-  const user = await User.findOne({ email }).select('+password');
-  if (!user || !(await user.correctPassword(password, user.password))) { // instance method
-    return next(new AppError('Incorrect email or password', 401));
+  const user = await User.findOne({ email }).select("+password");
+  if (!user || !(await user.correctPassword(password, user.password))) {
+    // instance method
+    return next(new AppError("Incorrect email or password", 200));//401
   }
 
   const token = signToken(user._id);
   res.status(200).json({
-    status: 'success',
+    status: "success",
     token,
   });
-  
+
   // // 3) If everything ok, send token to client
   // createSendToken(user, 200, res);
 });
@@ -54,13 +53,13 @@ exports.protect = catchAsync(async (req, res, next) => {
   let token;
   if (
     req.headers.authorization &&
-    req.headers.authorization.startsWith('Bearer')
+    req.headers.authorization.startsWith("Bearer")
   ) {
-    token = req.headers.authorization.split(' ')[1];
+    token = req.headers.authorization.split(" ")[1];
   }
   if (!token) {
     return next(
-      new AppError('You are not authorized to access this page', 401)
+      new AppError("You are not authorized to access this page", 401)
     );
   }
   //2 Verfication Token
@@ -68,7 +67,7 @@ exports.protect = catchAsync(async (req, res, next) => {
   //3 check if user still exist
   const freshUser = await User.findById(decoded.id);
   if (!freshUser) {
-    return next(new AppError('user has been deleted', 401));
+    return next(new AppError("user has been deleted", 401));
   }
   // check if user changed password
   freshUser.changedPasswordAfter(decoded.iat);
