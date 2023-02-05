00d392e8cab0fad2fda807cdf7e951d38f7d9e81
Ahmed AbdElsalam <91109569+Genius8545@users.noreply.github.com>
2023-02-05 17:54:02 +0200

    Update appError.js

--- utils/appError.js
+++ utils/appError.js
@@ -2,7 +2,7 @@ class AppError extends Error {
   constructor(message, statusCode) {
     super(message);
     this.statusCode = statusCode;
-    this.status = `${statusCode}`.startsWith('2') ? 'sucees' : 'error';
+    this.status = `${statusCode}`.startsWith('2') ? 'success' : 'error';
     this.isOperational = true;
     Error.captureStackTrace(this, this.constructor);
   }
