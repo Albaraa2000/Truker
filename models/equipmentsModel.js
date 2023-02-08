6b0cef1eadcf1a3943d6ca3c45d7f3e9d910d532
genius8545 <ahmedabdelsalm6@gmail.com>
2023-02-08 14:01:51 +0200

    edit equ model

--- models/equipmentsModel.js
+++ models/equipmentsModel.js
@@ -3,31 +3,27 @@ const mongoose = require("mongoose");
 const equipmentSchema = new mongoose.Schema({
   title: {
     type: String,
-    required: true,
+    required: [true, "please provide title"],
   },
   description: {
     type: String,
-    required: [true,"please provide description"],
-  },
-  avatar: {
-    type: String,
-    required: [true,"please provide avatar"],
+    required: [true, "please provide description"],
   },
   photo: {
     type: String,
-    required: [true,"please provide photo"],
+    required: [true, "please provide photo"],
   },
   type: {
     type: String,
-    required: true,
+    required: [true, "please provide  type"],
   },
   price: {
     type: Number,
-    required: [true,"please provide  price"],
+    required: [true, "please provide  price"],
   },
   rating: {
     type: Number,
-    required: [true,"please provide rating"],
+    required: [true, "please provide rating"],
   },
   favourite: {
     type: Boolean,
--- models/userModel.js
+++ models/userModel.js
@@ -1,59 +1,66 @@
-const mongoose = require('mongoose');
-const validator = require('validator');
-const bcrypt = require('bcryptjs');
+const mongoose = require("mongoose");
+const validator = require("validator");
+const bcrypt = require("bcryptjs");
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: [true, 'please tell us your name'],
+    required: [true, "please tell us your name"],
   },
   email: {
     type: String,
-    required: [true, 'please tell us your email address'],
+    required: [true, "please tell us your email address"],
     unique: true,
     lowercase: true,
-    validate: [validator.isEmail, 'Please provide a valid email'],
+    validate: [validator.isEmail, "Please provide a valid email"],
   },
   photo: {
     type: String,
   },
   password: {
     type: String,
-    required: [true, 'Please provide a valid password'],
+    required: [true, "Please provide a valid password"],
     minlength: 8,
-    select: false
+    select: false,
   },
   passwordConfirm: {
     type: String,
-    required: [true, 'Please confirm your password'],
-    validate:{
-      validator: function(el){
-        return el === this.password; 
+    required: [true, "Please confirm your password"],
+    validate: {
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: "Passwords do not match",
     },
-    message: 'Passwords do not match'
-  }},
+  },
+  avatar: {
+    type: String,
+  },
   passwordChangedAt: Date,
-  
 });
-userSchema.pre('save',async function(next){
-  if(!this.isModified('password')) return next();
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
   // to encrypt the password
-  this.password = await bcrypt.hash(this.password , 12)
+  this.password = await bcrypt.hash(this.password, 12);
   // to delete the password confirmation from the database
   this.passwordConfirm = undefined;
   next();
-})
+});
 // instance ====> method available in all documents
-userSchema.methods.correctPassword = async function (candidatePassword,userPassword) {
-  return await bcrypt.compare(candidatePassword,userPassword);
-}
+userSchema.methods.correctPassword = async function (
+  candidatePassword,
+  userPassword
+) {
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
 
 // instance
-userSchema.methods.changedPasswordAfter = function (JWTTimestamp){//this refere to current document
-  if(this.passwordChangedAt){
-    console.log(this.passwordChangedAt,JWTTimestamp);
+userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
+  //this refere to current document
+  if (this.passwordChangedAt) {
+    console.log(this.passwordChangedAt, JWTTimestamp);
   }
   return false;
-}
-const User = new mongoose.model('User', userSchema);
+};
+const User = new mongoose.model("User", userSchema);
 
 module.exports = User;
