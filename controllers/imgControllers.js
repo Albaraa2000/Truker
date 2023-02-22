cccdabdfa0ceaa9445f537546a70a795e07796e2
Ahmed AbdElsalam <91109569+Genius8545@users.noreply.github.com>
2023-02-22 16:47:52 +0000

    Update imgControllers.js

--- controllers/imgControllers.js
+++ controllers/imgControllers.js
@@ -1,9 +1,7 @@
 const path = require("path");
 const multer = require("multer");
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "images/");
-  },
+
   filename: (req, file, cb) => {
     console.log(file);
     cb(null, Date.now() + path.extname(file.originalname));
