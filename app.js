85774d6030378636992df4038b60a32b8729c295
genius8545 <ahmedabdelsalm6@gmail.com>
2023-03-04 16:54:12 +0000

    protect

--- app.js
+++ app.js
@@ -20,6 +20,10 @@ app.use(express.json());
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE, OPTIONS"
+  );
+  res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
