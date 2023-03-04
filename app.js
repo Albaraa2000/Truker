a75a8754bdf4000f37e7ec5db3f6ddc8425aa245
genius8545 <ahmedabdelsalm6@gmail.com>
2023-03-04 15:52:06 +0000

    token

--- app.js
+++ app.js
@@ -21,10 +21,11 @@ app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
   next();
 });
+
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/Equipments", equipmentsRouter);
 
